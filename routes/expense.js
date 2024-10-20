7754fa89640d9d980e3d00c267562e9ddf9016d5
Infinite Spiral <stephaneramael@live.co.uk>
2024-10-20 14:03:35 +0100

    created a PUT route

--- routes/expense.js
+++ routes/expense.js
@@ -4,14 +4,14 @@ const express = require('express');
 const router = express.Router();
 
 // require the middlewares and callback functions from the controller directory
-const { create, expenseById, read } = require('../controllers');
+const { create, expenseById, read, update } = require('../controllers');
 
 // Create POST route to create an expense
 router.post('/expense/create', create);
 // Create GET route to read an expense
 router.get('/expense/:id', expenseById, read);
 // Create PUT route to update an expense
-
+router.put('/expense/:id', expenseById, update);
 // Create DELETE route to remove an expense
 
 // Create GET route to read a list of expenses
